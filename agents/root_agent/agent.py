8cc566ff6b17ab624d37c2b5a6dae0355da7ce8c
HenryChang <henry930619@gmail.com>
2026-01-04 14:47:59 +0800

    feat: empower agent searching ability with google_search

--- agents/root_agent/agent.py
+++ agents/root_agent/agent.py
@@ -1,9 +1,11 @@
 from google.adk.agents import LlmAgent
+from google.adk.tools import google_search
 
 root_agent = LlmAgent(
     name="root_agent",
     description="A helpful assistant for user questions.",
     instruction="Answer user questions to the best of your knowledge",
     model="gemini-2.5-flash",
+    tools=[google_search],
 )
 
